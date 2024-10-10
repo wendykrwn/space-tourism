dd3394865d69cccc284a6aa98aaf2f763c439fe6
wendykrwn <wendykrwn@gmail.com>
2024-10-10 02:30:00 +0200

    destination desktop

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -8,34 +8,39 @@ import TabNav from "../ui/components/destination/tabNav";
 const Destination = () => {
     const destinationNames = destinations.map(val=>val.name)
     const [currDestIndex, setCurrDestIndex] = useState(0)
-    return <div className="p-6 min-h-screen flex flex-col" >
+    return (
+    <div className="p-6 h-full flex flex-col lg:max-w-[1110px] lg:px-0 mx-auto" >
         <div className="uppercase text-white flex justify-center md:justify-start tracking-[15%] gap-x-6">
             <span className="font-secondary font-bold text-[16px] text-white/30 md:text-[20px] ">01</span> <span className="font-secondary text-[16px] md:text-[20px]">Pick your destination</span>
         </div>
-        <div className="w-full flex justify-center items-center p-8">
-            <Image
-                src={destinations[currDestIndex].images.png}
-                alt={`image of ${destinations[0].name}`}
-                width={480}
-                height={480}
-                className="w-[150px] h-auto md:w-[300px]"
-            />
-        </div>
-        <div className="flex flex-col gap-y-6 h-full md:w-[514px] self-center ">
-            <TabNav destinationNames={destinationNames} currDestIndex={currDestIndex} handleIndexDest={setCurrDestIndex} />
-            <div>
-                <h2 className="text-preset-2 uppercase text-center">{destinations[currDestIndex].name}</h2>
-                <p className="text-center text-preset-9">
-                    {destinations[currDestIndex].description}
-                </p>
+        <div className="flex flex-col h-full lg:flex-row lg:justify-around items-center">
+            <div className="w-full lg:w-auto flex justify-center items-center p-8 lg:p-0">
+                <Image
+                    src={destinations[currDestIndex].images.png}
+                    alt={`image of ${destinations[0].name}`}
+                    width={480}
+                    height={480}
+                    className="w-[150px] h-auto md:w-[300px] lg:w-[480px]"
+                />
             </div>
-            <div className="w-full h-[1px] bg-white/25"/>
-            <div className="flex flex-col gap-y-8 md:flex-row md:justify-between">
-                <Statistic title="Avg. distance" data={destinations[currDestIndex].distance} />
-                <Statistic title="Est. travel time" data={destinations[currDestIndex].travel} />
+            <div className="flex flex-col gap-y-6 lg:gap-y-10 h-full  md:w-[514px] self-center lg:h-auto lg:w-full lg:max-w-[445px] ">
+                <TabNav destinationNames={destinationNames} currDestIndex={currDestIndex} handleIndexDest={setCurrDestIndex} />
+                <div>
+                    <h2 className="text-preset-2 uppercase text-center lg:text-left">{destinations[currDestIndex].name}</h2>
+                    <p className="text-center text-preset-9 lg:text-left transition-opacity duration-500 ease-in-out opacity-100">
+                        {destinations[currDestIndex].description}
+                    </p>
+                </div>
+                <div className="w-full h-[1px] bg-white/25"/>
+                <div className="flex flex-col gap-y-8 lg:gap-y-10 md:flex-row md:justify-between">
+                    <Statistic title="Avg. distance" data={destinations[currDestIndex].distance} />
+                    <Statistic title="Est. travel time" data={destinations[currDestIndex].travel} />
+                </div>
             </div>
         </div>
     </div>
+    )
+
 }
 
 export default Destination
--- src/app/ui/components/destination/tabNav.tsx
+++ src/app/ui/components/destination/tabNav.tsx
@@ -1,6 +1,6 @@
 const TabNav = ({destinationNames, currDestIndex, handleIndexDest}:{destinationNames:string[],currDestIndex:number, handleIndexDest: Function}) => {
     return (
-        <div className="flex justify-around preset-8 
+        <div className="flex justify-around lg:flex-row md:justify-center md:gap-x-8 lg:justify-start lg:gap-x-8 preset-8 lg:w-auto
         ">
             {
                 destinationNames?.map((destination,index) => <button onClick={()=>{handleIndexDest(index)}} className={`relative uppercase flex h-8 before:h-[3px] before:w-full before:absolute before:bottom-0 ${ destinationNames[currDestIndex] == destination ? "before:bg-white" : "hover:before:bg-white/50 before:bg-transparent"} `}>
