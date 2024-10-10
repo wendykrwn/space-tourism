650e88fbaa015e4970cabb1249371f6621e07c54
wendykrwn <wendykrwn@gmail.com>
2024-10-10 13:38:56 +0200

    add component for title

---
+++ src/app/ui/components/stepTitle.tsx
@@ -0,0 +1,9 @@
+const StepTitle = ({number, text}:{number:string, text:string}) => {
+    return (
+        <div className="uppercase text-white flex justify-center md:justify-start tracking-[15%] gap-x-6">
+            <span className="font-secondary font-bold text-[16px] text-white/30 md:text-[20px] ">{number}</span> <span className="font-secondary text-[16px] md:text-[20px]">{text}</span>
+        </div>
+    )
+}
+
+export default StepTitle
