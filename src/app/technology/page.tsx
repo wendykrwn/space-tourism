ed9b651c419c1c827dd3d3424063f2ebfd3d5700
wendykrwn <wendykrwn@gmail.com>
2024-10-14 23:57:03 +0200

    technology desktop

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -10,23 +10,30 @@ import Explanation from "../ui/components/explanation";
 const Technology = ()=> {
     const [currTechIndex, setCurrTechIndex] = useState(0)
     return (
-        <div className="py-6">
-            <div className="px-6">
+        <div className="py-6 lg:max-w-[1275px] ml-auto ">
+            <div className="px-6 md:mb-5 lg:px-0">
                 <StepTitle number="03" text="Space launch 101"/>
             </div>
-            <div className="grid grid-rows-2 h-full">
-                <div className="flex items-center h-[322px]">
+            <div className="grid grid-rows-2 lg:grid-rows-none lg:grid-cols-2 h-full lg:items-center">
+                <div className="flex items-center h-[322px] md:h-auto md:mb-5 order-2">
                     <Image
                         src={technology[currTechIndex].images.landscape}
                         alt={`image of ${technology[currTechIndex]?.name}`}
                         width={500}
                         height={500}
-                        className="w-full h-[258px]"
+                        className="w-full h-[258px] md:h-[357px] lg:hidden "
+                    />
+                    <Image
+                        src={technology[currTechIndex].images.portrait}
+                        alt={`image of ${technology[currTechIndex]?.name}`}
+                        width={500}
+                        height={500}
+                        className="w-full hidden lg:block "
                     />
                 </div>
-                <div className="px-6 flex flex-col gap-10 ">
+                <div className="px-6 flex flex-col lg:flex-row gap-10 lg:order-1 h-fit ">
                     <PaginationWithNumber currIndex={currTechIndex} setCurrIndex={setCurrTechIndex} numberOfPage={technology.length} />
-                    <Explanation className="text-center" subtitle="The Terminology..." title={technology[currTechIndex].name} description={technology[currTechIndex].description} />
+                    <Explanation className="text-center self-center max-w-[512px]" subtitle="The Terminology..." title={technology[currTechIndex].name} description={technology[currTechIndex].description} />
                 </div>
             </div>
         </div>
--- src/app/ui/components/technology/paginationWithNumber.tsx
+++ src/app/ui/components/technology/paginationWithNumber.tsx
@@ -1,13 +1,13 @@
 const PaginationWithNumber = ({currIndex,setCurrIndex, numberOfPage}:{currIndex : number, numberOfPage:number, setCurrIndex: Function}) => {
     return (
-        <div className="flex justify-center gap-x-4">
+        <div className="flex justify-center gap-x-4 lg:flex-col lg:justify-between">
         {
             [...Array(numberOfPage)].map((val,index)=>{
                 return (
                     <button 
                         key={index}
                         onClick={()=>{setCurrIndex(index)}}
-                        className={`w-[40px] h-[40px] cursor-pointer border-solid border-[1px] transition-all duration-300 hover:border-white rounded-full ${currIndex == index ? "bg-white text-background border-white " : "bg-transparent hover:bg-white hover:text-background hover:border-w border-white/25 "}`}
+                        className={`w-[40px] h-[40px] lg:w-[80px] lg:h-[80px] cursor-pointer border-solid border-[1px] transition-all duration-300 hover:border-white rounded-full ${currIndex == index ? "bg-white text-background border-white " : "bg-transparent hover:bg-white hover:text-background hover:border-w border-white/25 "}`}
                     >
                         {index + 1}
                     </button>
